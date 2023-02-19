cdc1a9448c4369d0c1b629474e089889577c78af
sinancansevinc <sinancan_sevinc@hotmail.com>
2023-02-19 19:45:17 +0300

    Seeding data added !

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,18 @@
+﻿namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var platformService = serviceScope.ServiceProvider.
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+
+        }
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => 
 options.UseInMemoryDatabase("InMem"));
 
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -27,5 +29,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+PrepDb.PrepPopulation(app);
 
 app.Run();
